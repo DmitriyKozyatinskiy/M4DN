d448f25a721b19f5facff85a653256ba5a9f51c6
DmitriyKozyatinskiy <dmitriy.kozyatinskiy@gmail.com>
2017-02-20 11:58:20 +0200

    db

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -39,10 +46,10 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        'mysql_local' => [
             'driver' => 'mysql',
-            // 'host' => env('DB_HOST', '127.0.0.1'),
-            'host' => '127.0.0.1',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            //'host' => '127.0.0.1',
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -53,6 +60,17 @@ return [
             'strict' => true,
             'engine' => null,
         ],
+
+        'mysql' => array(
+          'driver'    => 'mysql',
+          'host'      => $host,
+          'database'  => $database,
+          'username'  => $username,
+          'password'  => $password,
+          'charset'   => 'utf8',
+          'collation' => 'utf8_unicode_ci',
+          'prefix'    => '',
+        ),
 
         'pgsql' => [
             'driver' => 'pgsql',

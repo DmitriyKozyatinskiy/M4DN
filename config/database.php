3f99ffbc60b3521da9ff867d01351cebe3dfafc9
DmitriyKozyatinskiy <dmitriy.kozyatinskiy@gmail.com>
2017-02-26 20:58:48 +0200

    Fix db config for heroku

--- config/database.php
+++ config/database.php
@@ -1,11 +1,11 @@
 <?php
 
-//$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-//
-//$host = $url["host"];
-//$username = $url["user"];
-//$password = $url["pass"];
-//$database = substr($url["path"], 1);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 
 return [
 
@@ -46,7 +46,7 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        'mysql_local' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             //'host' => '127.0.0.1',
@@ -61,16 +61,16 @@ return [
             'engine' => null,
         ],
 
-//        'mysql_heroku' => array(
-//          'driver'    => 'mysql',
-//          'host'      => $host,
-//          'database'  => $database,
-//          'username'  => $username,
-//          'password'  => $password,
-//          'charset'   => 'utf8',
-//          'collation' => 'utf8_unicode_ci',
-//          'prefix'    => '',
-//        ),
+        'mysql' => array(
+          'driver'    => 'mysql',
+          'host'      => $host,
+          'database'  => $database,
+          'username'  => $username,
+          'password'  => $password,
+          'charset'   => 'utf8',
+          'collation' => 'utf8_unicode_ci',
+          'prefix'    => '',
+        ),
 
         'pgsql' => [
             'driver' => 'pgsql',

ac60fbab42924ccf9810b4cf6b3e20cc372f4d03
DmitriyKozyatinskiy <dmitriy.kozyatinskiy@gmail.com>
2017-03-10 01:47:11 +0200

    Deploy

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,6 @@
 const { mix } = require('laravel-mix');
 
-mix.webpackConfig({
-  'presets': [
-    'es2015',
-  ]
-});
+mix.webpackConfig({});
 
 /*
  |--------------------------------------------------------------------------

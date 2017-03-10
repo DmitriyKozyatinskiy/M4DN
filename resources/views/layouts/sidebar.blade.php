d56a3ea57eaf576ab835df3010c6712ba639462f
DmitriyKozyatinskiy <dmitriy.kozyatinskiy@gmail.com>
2017-03-10 04:32:38 +0200

    Add download url

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,11 +1,11 @@
 <div class="Sidebar">
-  <a href="{{ url('history')  }}" class="Sidebar__Item">History</a>
-  <a href="{{ url('devices/show')  }}" class="Sidebar__Item">Devices</a>
-  <a href="#" class="Sidebar__Item">Downloads</a>
-  <a href="{{ url('account/subscription')  }}" class="Sidebar__Item">Subscription</a>
-  <a href="{{ url('account/settings')  }}" class="Sidebar__Item">Account Settings</a>
+  <a href="{{ url('history') }}" class="Sidebar__Item">History</a>
+  <a href="{{ url('devices/show') }}" class="Sidebar__Item">Devices</a>
+  <a href="{{ url('downloads') }}" class="Sidebar__Item">Downloads</a>
+  <a href="{{ url('account/subscription') }}" class="Sidebar__Item">Subscription</a>
+  <a href="{{ url('account/settings') }}" class="Sidebar__Item">Account Settings</a>
 
   {{--@if (Auth::check() && Auth::user()->isAdmin)--}}
-    {{--<a href="#" class="Sidebar__Item">Admin Panel</a>--}}
+  {{--<a href="#" class="Sidebar__Item">Admin Panel</a>--}}
   {{--@endif--}}
 </div>

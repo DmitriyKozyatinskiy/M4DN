b1a0c3932a5808d02cc3725dad9db926754ea0d9
DmitriyKozyatinskiy <dmitriy.kozyatinskiy@gmail.com>
2017-03-30 23:39:14 +0300

    Implement ads

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="Header">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-xs-4 col-sm-3 col-md-offset-1">
+      <div class="col-xs-4 col-sm-3 col-md-offset-1 col-lg-2">
         <a href="#" id="js-tray-trigger" class="Tray_TriggerContainer navbar-brand">
           <img alt="Sneekr" title="Sneekr" src="{{ secure_asset('images/tray.png')}}" class="Tray__Trigger js-tray-trigger">
         </a>
@@ -21,7 +21,7 @@
         </a>
       </div>
 
-      <div class="Header__AdContainer col-xs-8 col-sm-5 col-md-4">
+      <div class="Header__AdContainer col-xs-8 col-sm-5 col-md-4 col-lg-6">
         <div style="width: 100%;">
           <ins class="adsbygoogle"
                style="display:block"
@@ -33,7 +33,7 @@
       </div>
 
 
-      <div class="Header__DropdownContainer collapse in col-sm-4 col-md-3">
+      <div class="Header__DropdownContainer collapse in col-sm-4 col-md-3 col-lg-2">
         <ul class="nav navbar-nav pull-right">
           @if (Auth::guest())
             <li><a class="Header__Link" href="{{ secure_url('/login') }}">Login</a></li>
